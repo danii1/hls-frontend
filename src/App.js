383edfbfb872975b00896584448c27f1d7d0da54
Daniil Pokrovsky <daniil.pokrovsky@gmail.com>
2021-01-22 14:37:11 +0300

    Play HLS streams

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef } from "react";
+const host = "http://192.168.1.53:8000";
+const Hls = window.Hls;
+
+const config = {
+  xhrSetup: function (xhr, url) {
+    if (url.includes("/keys/"))
+      xhr.setRequestHeader("x-playback-session-id", "123");
+  },
+};
 
 function App() {
+  var videoEl = useRef(null);
+
+  useEffect(() => {
+    var videoSrc = `${host}/streams/8bc495dd387d130be25aaae097785cda.m3u8`;
+    //
+    // First check for native browser HLS support
+    //
+    if (videoEl.current.canPlayType("application/vnd.apple.mpegurl")) {
+      videoEl.current.src = videoSrc;
+      //
+      // If no native HLS support, check if hls.js is supported
+      //
+    } else if (Hls.isSupported()) {
+      var hls = new Hls(config);
+      hls.loadSource(videoSrc);
+      hls.attachMedia(videoEl.current);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        playsInline
+        id="video"
+        ref={videoEl}
+        controls
+        width="300px"
+        height="60px"
+      />
     </div>
   );
 }
